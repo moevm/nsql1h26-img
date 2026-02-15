289090926d13d182870c11037869f7b04cb326ad
Talismanch1k <talismanch1k@yandex.ru>
2026-02-15 23:01:54 +0300

    fix: change url style and name convention

--- hello_world/db.py
+++ hello_world/db.py
@@ -1,15 +1,15 @@
 from functools import lru_cache
 from urllib.parse import quote_plus
-from pydantic import Field
+from pydantic import Field, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class MongoDBConfig(BaseSettings):
     host: str = Field(alias="MONGO_HOST")
     port: int = Field(alias="MONGO_PORT")
-    db: str = Field(alias="MONGO_DB")
     user: str = Field(alias="MONGO_USER")
     password: str = Field(alias="MONGO_PASSWORD")
+    db_name: str = Field(alias="MONGO_DB_NAME")
 
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -17,11 +17,13 @@ class MongoDBConfig(BaseSettings):
         extra="ignore",
     )
 
-    def get_mongo_url(self) -> str:
+    @computed_field
+    @property
+    def mongo_url(self) -> str:
         user = quote_plus(self.user)
         password = quote_plus(self.password)
 
-        return f"mongodb://{user}:{password}@{self.host}:{self.port}/{self.db}"
+        return f"mongodb://{user}:{password}@{self.host}:{self.port}/{self.db_name}"
 
 
 @lru_cache
@@ -31,4 +33,4 @@ def get_config() -> MongoDBConfig:
 
 if __name__ == "__main__":
     config = get_config()
-    print("MongoDB URL:", config.get_mongo_url())
+    print("MongoDB URL:", config.mongo_url)
