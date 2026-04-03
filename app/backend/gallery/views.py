3f5a619b720f1223db8f474a7de987049ecf71f6
EnikeevAnton <enikeev.anton2033c@yandex.ru>
2026-04-03 13:34:20 +0300

    feature: implement REST API for gallery with advanced filtering and search

--- app/backend/gallery/views.py
+++ app/backend/gallery/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, viewsets
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+from .filters import ImageFilter
+from .models import Image
+from .permissions import IsAuthorOrAdminOrReadOnly
+from .serializers import ImageSerializer, ImageUpdateSerializer
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrAdminOrReadOnly,
+    ]
+
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = ImageFilter
+    search_fields = ["title"]
+
+    def get_serializer_class(self):
+        if self.action in ["update", "partial_update"]:
+            return ImageUpdateSerializer
+        return ImageSerializer
+
+    def perform_create(self, serializer) -> None:
+        serializer.save(author=self.request.user)
