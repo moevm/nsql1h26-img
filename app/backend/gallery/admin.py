1fb151a98e092be9d78cdee8096bfd6e015afe10
EnikeevAnton <enikeev.anton2033c@yandex.ru>
2026-04-03 13:32:51 +0300

    feature: implement Image model and admin interface

--- app/backend/gallery/admin.py
+++ app/backend/gallery/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "image_format",
+        "file_size_mb",
+        "width",
+        "height",
+        "author",
+    )
+    list_filter = ("image_format", "created_at")
+    readonly_fields = ("image_format", "width", "height", "file_size_mb")
